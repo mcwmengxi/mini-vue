4b5763fa11710578fffd0908871c9637a536f2c5
luo <1395568275@qq.com>
2022-03-03 22:27:01 +0800

    实现初始化element主流程

--- example/hello/App.js
+++ example/hello/App.js
@@ -3,7 +3,16 @@ import { h } from "../../lib/guide-mini-vue.esm.js"
 export const App = {
   render() {
     // ui
-    return h("div", "hi, " + this.msg);
+    return h(
+      "div",
+      {
+        id:"root",
+        class:["green","bold"],
+      },
+      
+      [h("p",{class:"green"},"hi"),h("p",{class:"blue"},"nangong")]
+      //  '',"hi, " + this.msg
+       );
   },
 
   setup() {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from './../shared/index';
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode:any, container:any){
@@ -5,7 +6,17 @@ export function render(vnode:any, container:any){
 }
 
 function patch(vnode,container){
-  processComponent(vnode,container)
+  // 原生标签
+  if(typeof vnode.type === 'string'){
+    processElement(vnode,container)
+  }else if(isObject(vnode.type)){
+    // 处理组件
+    processComponent(vnode,container)
+  }
+}
+
+function processElement(vnode,container){
+  mountElement(vnode,container)
 }
 
 function processComponent(vnode,container){
@@ -19,6 +30,32 @@ function mountcomponent(vnode,container){
   setupRenderEffect(instance,container)
 }
 
+function mountElement(vnode,container){
+  // 创建真实dom
+
+  const el = document.createElement(vnode.type)
+  const { children, props } = vnode
+  // 处理子节点，是文本还是标签
+  if(typeof children === 'string'){
+    el.textContent = children
+  }else if(Array.isArray(children)){
+    mountChildren(vnode,el)
+  }
+
+  // 属性
+  for(const key in props){
+    el.setAttribute(key,props[key])
+  }
+  // 添加到根标签上
+  container.append(el)
+}
+
+function mountChildren(vnode,container){
+  vnode.children.forEach(v => {
+    // 递归添加子标签
+    patch(v,container)
+  });
+}
 function setupRenderEffect(instance,container){
   // 调用组件实例的render()
   let subTree
